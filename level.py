dfa348cdfa990dda1b5c90bbdd938510dc9f36d9
Nikita <117333817+SanctusBebrus@users.noreply.github.com>
2023-01-16 11:35:15 +1000

    some code fixes

--- level.py
+++ level.py
@@ -6,6 +6,7 @@ import random
 
 # Teams
 default_team = player.Team((255, 255, 255))
+
 YELLOW = player.Team((255, 215, 0))
 GREEN = player.Team((0, 128, 0))
 RED = player.Team((255, 0, 0))
@@ -14,7 +15,7 @@ BLUE = player.Team((0, 0, 255))
 default_field = list(
     map(lambda y: list(map(lambda x: Cell(default_team), range(LENGTH))), range(WIDTH)))
 
-for i in range(random.randint(4, 8)):
+for i in range(random.randint(6, 9)):
     x1 = random.randint(2, LENGTH - 3)
     y1 = random.randint(2, LENGTH - 3)
     default_field[x1][y1] = IceCell(default_team)
@@ -42,33 +43,29 @@ base = units.Base
 mine = units.Mine
 
 # YELLOW Team
-for i in range(2):
-    for j in range(LENGTH - 2, LENGTH):
-        default_field[i][j].set_team(YELLOW)
+for i, j in product(range(2), range(LENGTH - 2, LENGTH)):
+    default_field[i][j].set_team(YELLOW)
 
 default_field[0][LENGTH - 1].set_unit(base(YELLOW))
 default_field[1][LENGTH - 2].set_unit(rover(YELLOW))
 
 # GREEN Team
-for i in range(2):
-    for j in range(2):
-        default_field[i][j].set_team(GREEN)
+for i, j in product(range(2), range(2)):
+    default_field[i][j].set_team(GREEN)
 
 default_field[0][0].set_unit(base(GREEN))
 default_field[1][1].set_unit(rover(GREEN))
 
 # BLUE Team
-for i in range(LENGTH - 2, LENGTH):
-    for j in range(LENGTH - 2, LENGTH):
-        default_field[i][j].set_team(BLUE)
+for i, j in product(range(LENGTH - 2, LENGTH), range(LENGTH - 2, LENGTH)):
+    default_field[i][j].set_team(BLUE)
 
 default_field[LENGTH - 1][LENGTH - 1].set_unit(base(BLUE))
 default_field[LENGTH - 2][LENGTH - 2].set_unit(rover(BLUE))
 
 # RED Team
-for i in range(LENGTH - 2, LENGTH):
-    for j in range(2):
-        default_field[i][j].set_team(RED)
+for i, j in product(range(LENGTH - 2, LENGTH), range(2)):
+    default_field[i][j].set_team(RED)
 
 default_field[LENGTH - 1][0].set_unit(base(RED))
 default_field[LENGTH - 2][1].set_unit(rover(RED))
